c5eb7d52ccf900a4234ce9ab5f62050eed1f42c6
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-11-13 15:45:41 +0100

    Add JWT for each new user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -29,7 +29,16 @@ router.post('/register', (req, res) => {
             if (err) throw err;
             newUser.password = hash;
             newUser.save()
-              .then(user => res.json(user))
+              .then(user => {
+                const payload = { id: user.id, handle: user.handle };
+
+                jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                  res.json({
+                    success: true,
+                    token: 'Bearer ' + token
+                  });
+                });
+              })
               .catch(err => console.log(err));
           })
         })
