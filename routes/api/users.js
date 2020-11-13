22a53968fbd49ebd2a2c7bab974abbeb1b406dae
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-11-13 13:08:59 +0100

    Create new Login Post Route

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
-router.get('/test', (req, res) => res.json({ msg: "This is the users route" }));
+router.get('/test', (req, res) => res.json({ msg: 'This is the users route' }));
 
 router.post('/register', (req, res) => {
   User.findOne({ email: req.body.email })
     .then(user => {
-      if (user) return res.status(400).json({email: "A user has already registered with this address"})
+      if (user) return res.status(400).json({ email: 'A user has already registered with this address' });
       else {
         const newUser = new User({
           handle: req.body.handle,
@@ -30,6 +30,22 @@ router.post('/register', (req, res) => {
           })
         })
       }
+    })
+});
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) return res.status(404).json({ email: 'This user doed not exist' });
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) res.json({ msg: 'Success' });
+          else return res.status(400).json({ password: 'Incorrect password' });
+        })
     })
 });
 
