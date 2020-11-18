435b922facf2d579d676d4b732a53771cbf6c206
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-11-18 17:51:17 +0100

    Setup User Logout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import jwt_decode from 'jwt-decode'; 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from './App';
+import Root from './components/root';
+import configureStore from './store/store';
+import { setAuthToken } from './util/session_api_util';
+import { logout } from './actions/session_actions';
+
+import './index.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+
+  if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+
+    const decodeUser = jwt_decode(localStorage.jwtToken);
+    const preloadedState = { session: { isAuthenticated: true, user: decodeUser } };
+
+    store = configureStore(preloadedState);
+
+    const currentTime = Date.now() / 1000;
+
+    if (decodeUser.exp < currentTime) {
+      store.dispatch(logout());
+      window.location.href = '/login'
+    }
+  } else {
+    store = configureStore({});
+  }
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
+});
