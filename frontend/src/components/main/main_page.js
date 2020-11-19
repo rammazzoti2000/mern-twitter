a812ccff6a0b2f3f2252c7d3deda59fb21800ecc
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-11-19 16:44:00 +0100

    Create NavBar component with Links

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -7,7 +7,7 @@ class MainPage extends React.Component {
       <div>
         <h1>A Twitter Clone</h1>
         <footer>
-          Copyright &copy; 2020 ale[x360]
+          Copyright &copy; 2020 ale[x360] - Chirper
         </footer>
       </div>
     );
