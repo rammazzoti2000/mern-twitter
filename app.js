55465b1697c6d85fe7017a0119acbb449feaff85
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-12-02 21:30:07 +0100

    Fixing Heroku

--- app.js
+++ app.js
@@ -10,13 +10,6 @@ const tweets = require("./routes/api/tweets");
 
 const path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
-
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB successfully'))
@@ -31,7 +24,13 @@ app.use(bodyParser.json());
 app.use('/api/users', users);
 app.use('/api/tweets', tweets);
 
-const port = process.env.PORT || 5000;
-const host = '0.0.0.0';
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
-app.listen(port, host, () => console.log(`Server is running on port ${port}`));
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
