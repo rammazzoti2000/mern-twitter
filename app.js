9d9d7b8188ea76d4df8fec5dc6cf7419e32b42f9
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-12-02 19:29:54 +0100

    Configure Heroku Deployment

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.use(bodyParser.json());
 app.use('/api/users', users);
 app.use('/api/tweets', tweets);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
