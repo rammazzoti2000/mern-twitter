909ae7f15f4001f5617e4af0f24d8bacd3770f16
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-12-02 21:25:16 +0100

    Fixing Heroku

--- config/keys.js
+++ config/keys.js
@@ -1,2 +1,5 @@
-if (process.env.MODE_ENV === 'production') module.exports = require('./keys_prod');
-else module.exports = require('./keys_dev');
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
